26a7583147887fb665233f965b9e8db49a3cac5b
Cristian Moldovan <crissmoldovan@gmail.com>
2018-09-09 08:55:12 +0300

    refactored to new libs generator model

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,14 @@
-import { Generator, FileSet } from '@teleporthq/teleport-lib-js'
-import RNComponentGenerator from './generators/component'
-import RNProjectGenerator from './generators/project'
+import { Generator, ComponentCodeGenerator } from '@teleporthq/teleport-lib-js'
 
-type GeneratorPlugin = (output: string) => string
+import DefaultRNComponentRenderer from './default'
 
-export default class TeleportGeneratorRN extends Generator {
-  public componentGenerator: RNComponentGenerator
-  public projectGenerator: RNProjectGenerator
-  public plugins: {
-    [key: string]: GeneratorPlugin
-  } = {}
+export default class TeleportGeneratorReactNative extends Generator {
+  constructor(name?: string, targetName?: string, customComponentRenderers?: { [key: string]: ComponentCodeGenerator }) {
+    const componentRenderers = {
+      default: new DefaultRNComponentRenderer(),
+      ...customComponentRenderers,
+    }
 
-  constructor() {
-    super('react-native-generator', 'react-native')
-
-    this.componentGenerator = new RNComponentGenerator(this)
-    this.projectGenerator = new RNProjectGenerator(this, this.componentGenerator)
-  }
-
-  public generateComponent<T, U>(component: T, options: U): FileSet {
-    return this.componentGenerator.generate(component, options)
-  }
-
-  public generateProject(component: any, options: any): FileSet {
-    return this.projectGenerator.generate(component, options)
-  }
-
-  public usePlugin(pluginName: string, plugin: GeneratorPlugin) {
-    if (this.plugins[pluginName]) throw new Error(`Plugin has \`${pluginName}\` has already been registered`)
-
-    this.plugins[pluginName] = plugin
+    super(name || 'react-native-generator', targetName || 'react-native', componentRenderers)
   }
 }
